bbf9e74f0ecab957487479cc55bd61aeaf25ee48
Cengizhan Şahin <cengizhan.sahin@id3.com.tr>
2022-07-20 11:59:05 +0300

    NWSP-1

---
+++ src/main/java/tr/com/id3/news/newsfetch/NewsFetchApplication.java
@@ -0,0 +1,13 @@
+package tr.com.id3.news.newsfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsFetchApplication.class, args);
+	}
+
+}
